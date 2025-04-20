cfb31cedfc5b73776425c2cae33e03075e8b8fbb
flizze <alexandre.bel@ynov.com>
2025-04-20 19:49:59 +0200

    res 1

--- waybar/scripts/power-menu.sh
+++ waybar/scripts/power-menu.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Make sure this runs with full environment
-export PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:$PATH"
-
 pkill -x rofi || true
 
-power_icon="../../img/power.png"
-restart_icon="../../img/restart.png"
-logout_icon="../../img/logout.png"
-sleep_icon="../../img/sleep.png"
+power_icon="~/confs/img/power.png"
+restart_icon="~/confs/img/restart.png"
+logout_icon="~/confs/img/logout.png"
+sleep_icon="~/confs/img/sleep.png"
 
 rofi_menu() {
     echo -en "Power\0icon\x1f$power_icon\n"
